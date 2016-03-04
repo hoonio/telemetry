a375107511123db600a440d49559b4bb5bd2776d
Hoonio <hoonio@hoonio.com>
2016-03-04 23:09:56 +0000

    websocket to connect to EF server

--- app.js
+++ app.js
@@ -4,42 +4,78 @@ dashboard.controller('Ctrl', ['$scope', 'socket',
   function($scope, socket) {
     $scope.name = "Aircraft dashboard";
 
-    $scope.message = '';
+    $scope.values = {
+      "control": {
+        "landing_gear": 0,
+        "flaps": 0
+      },
+      "telemetry": {
+        "altitude": 0,
+        "airspeed": 0
+      }
+    };
     $scope.messages = [];
     // When we see a new msg event from the server
-    socket.on('new:msg', function(message) {
-      $scope.messages.push(message);
-    });
+    // socket.on('new:msg', function(message) {
+    //   $scope.messages.push(message);
+    // });
     // Tell the server there is a new message
-    $scope.broadcast = function() {
-      socket.emit('broadcast:msg', {
-        message: $scope.message
-      });
-      $scope.messages.push($scope.message);
-      $scope.message = '';
+    $scope.connect = function() {
+    // socket.init();
+      wsUri = 'ws://ec2-54-228-248-101.eu-west-1.compute.amazonaws.com:8888/telemetry';
+      websocket = new WebSocket(wsUri);
+      console.log('connect to websocket')
+      websocket.onmessage = function(evt){
+        // console.log('message received: ' + $scope.messages);
+        // $scope.messages.push(evt.data);
+        updateValues(evt.data);
+      }
+
+    function updateValues(newData) {
+      console.log('write data: '+ newData)
+      if (newData == 'hello, world'){
+        $scope.name = 'Connection established'
+      }
+      else {
+        $scope.values = JSON.parse(newData);      
+      }
+      $scope.$apply();
+    }
+
+    //   socket.emit('broadcast:msg', {
+    //     message: $scope.message
+    //   });
+    //   $scope.messages.push($scope.message);
+    //   $scope.message = '';
     };
 }]);
 
 dashboard.factory('socket', function ($rootScope) {
-  var socket = io.connect('http://ec2-54-228-248-101.eu-west-1.compute.amazonaws.com:8888/telemetry');
+  var wsUri = 'ws://ec2-54-228-248-101.eu-west-1.compute.amazonaws.com:8888/telemetry';
+  // var socket = io.connect(' ws://ec2-54-228-248-101.eu-west-1.compute.amazonaws.com:8888/telemetry');
   return {
-    on: function(eventName, callback) {
-      socket.on(eventName, function() {
-        var args = arguments;
-        $rootScope.$apply(function() {
-          callback.apply(socket, args);
-        });
-      });
+    init: function() {
+      websocket = new WebSocket(wsUri);
+      websocket.onmessage = function(evt) { this.update(evt) };
+      // socket.on(eventName, function() {
+      //   var args = arguments;
+      //   $rootScope.$apply(function() {
+      //     callback.apply(socket, args);
+      //   });
+      // });
     },
-    emit: function(eventName, data, callback) {
-      socket.emit(eventName, data, function() {
-        var args = arguments;
-        $rootScope.$apply(function() {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
+    update: function(evt) {
+      console.log('message received: ' + evt.data);
+      $scope.messages.push(evt.data);
+
+      // socket.emit(eventName, data, function() {
+      //   var args = arguments;
+      //   $rootScope.$apply(function() {
+      //     if (callback) {
+      //       callback.apply(socket, args);
+      //     }
+      //   });
+      // })
     }
   };
 });
